f8ed42458f7582a8bc0e397ffc06b559428640af
kaplimonadi <kapilmonadi@gmail.com>
2023-02-12 17:03:49 +0530

    Formatting changes

--- src/com/kapil/sealed/OfflineEducation.java
+++ src/com/kapil/sealed/OfflineEducation.java
@@ -1,6 +1,6 @@
 package com.kapil.sealed;
 
-public final class OfflineEducation implements Education{
+public final class OfflineEducation implements Education {
     @Override
     public void conductClass() {
         System.out.println("Hello there from Offline Education");
